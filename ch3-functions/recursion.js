e9201002592203224ae38ac7296bdc837edcd916
Tessa Power <tessa@tessapower.co>
2020-06-24 13:58:33 +1200

    Add exercises from chapter 3

---
+++ ch3-functions/recursion.js
@@ -0,0 +1,18 @@
+/* Define a recursive function isEven corresponding to this description.
+ * The function should accept a single parameter (a positive, whole number) 
+ * and return a Boolean. Test it on 50 and 75. See how it behaves on -1.
+ * Why? Can you think of a way to fix this?
+ */
+
+function isEven(x) {
+    if (x == 0) {
+        return true;
+    } else if (x == 1) {
+        return false;
+    } else if (x < 0) {
+        return(isEven(-x));
+    } else {
+        return (isEven(x-2));
+    }
+}
+
