587ef42017ec817914f8fbdfd42968cdcff27409
Tessa Power <tessa@tessapower.co>
2021-01-25 20:55:21 +1300

    Fix module exports and tests for chapter 8 exercise

--- tests/retry.test.js
+++ tests/retry.test.js
@@ -1,5 +1,6 @@
 const retry = require('../ch8-bugs/retry');
 
-test('only return result if call succeeds', () => {
-    let result = retry.reliableMultiply(8, 8);
+test('only return result if call succeeds', async () => {
+  const result = await retry.reliableMultiply(8, 8);
+  expect(result).toBe(64);
 });
