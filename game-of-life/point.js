2d949f73f00b1505dcc89af2bb0f6e32a3839b6e
Tessa Power <tessa@tessapower.co>
2021-02-06 21:44:14 +1300

    Create point class

---
+++ game-of-life/point.js
@@ -0,0 +1,51 @@
+class Point {
+  constructor({x: x, y: y}) {
+    if (typeof x == "number" && typeof y == "number"){
+      x = Math.trunc(x);
+      y = Math.trunc(y);
+      this.x = x;
+      this.y = y;
+    } else {
+      throw new Error("Error: point can only consist of numbers");
+    }
+  }
+
+  get neighbors() {
+    return [this.up, this.topRight, this.right,  this.bottomRight, this.down, this.bottomLeft, this.left, this.topLeft];
+  }
+  
+  get up() {
+    return new Point({x: this.x, y: this.y + 1});
+  }
+
+  get topRight()
+  {
+    return new Point({x: this.x + 1, y: this.y + 1});
+  }
+
+  get right() {
+    return new Point({x: this.x + 1, y: this.y});
+  }
+
+  get bottomRight() {
+    return new Point({x: this.x + 1, y: this.y - 1});
+  }
+
+  get down() {
+    return new Point({x: this.x, y: this.y - 1});
+  }
+
+  get bottomLeft() {
+    return new Point({x: this.x - 1, y: this.y - 1});
+  }
+
+  get left() {
+    return new Point({x: this.x - 1, y: this.y });
+  }
+  
+  get topLeft() {
+    return new Point({x: this.x - 1, y: this.y + 1});
+  }
+}
+
+module.exports = { Point };
